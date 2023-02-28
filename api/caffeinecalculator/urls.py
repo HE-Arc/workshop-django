c0f582c6253d31f51b252e3651011648e9efcc2d
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2023-02-28 11:32:49 +0100

    update URLs

--- api/caffeinecalculator/urls.py
+++ api/caffeinecalculator/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/", include("caffeinecalculatorapp.urls")),
-    path("api-auth/", include("rest_framework.urls")),
+    path("api/api-auth/", include("rest_framework.urls")),
 ]
